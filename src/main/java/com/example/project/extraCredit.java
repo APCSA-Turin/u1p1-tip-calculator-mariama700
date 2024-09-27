b45508b34c5d9c00373310821750c3e757b54748
mariama700 <mariama70@nycstudents.net>
2024-09-27 03:52:59 +0000

    tip calculator

--- src/main/java/com/example/project/extraCredit.java
+++ src/main/java/com/example/project/extraCredit.java
@@ -1,6 +1,7 @@
 package com.example.project;
+import java.util.Scanner;
 
-public class TipCalculator {
+public class extraCredit {
     //WRITE YOUR PROGRAM IN calculateTip
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
@@ -8,25 +9,18 @@ public class TipCalculator {
 
         // I learned how to round from here: https://www.educative.io/answers/how-to-use-the-java-mathround-method
 
-        // calculating tip and rounding
+        // Calculating important values:
         double tip = cost * (percent/100.0);
         tip = Math.round(tip * 100) / 100.0;
 
-        // calculating the cost per person before tip
+        double costAndTip = tip + cost;
+        double costPerPersonAfterTip = Math.round((costAndTip / people) * 100) / 100.0;
+
         double costPerPersonBeforeTip = Math.round((cost / people) * 100) / 100.0;
 
-        // calculating total cost and tip together
-        double costAndTip = tip + cost;
-
-        // calculating the tip per person
         double tipPerPerson = tip / people;
         tipPerPerson = Math.round(tipPerPerson * 100) / 100.0;
-        
-        // calculating the cost per person after tip
-        double costPerPersonAfterTip = (cost + (cost * ((double) percent / 100))) / people;
-        costPerPersonAfterTip = Math.round(costPerPersonAfterTip * 100.0) / 100.0;
-                   
-        // running code
+                      
         result.append("-------------------------------\n");
         result.append("Total bill before tip: $" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
         result.append("Total percentage: " + percent + "%" + "\n");
@@ -36,15 +30,33 @@ public class TipCalculator {
         result.append("Tip per person: $" + tipPerPerson + "\n");
         result.append("Total cost per person: $" + costPerPersonAfterTip + "\n");
         result.append("-------------------------------\n");
+        
+        // This is where I add extra credit
+        String items = "";
 
+        boolean endCode = false;
+        Scanner scan = new Scanner(System.in);
+
+        while (endCode == false){
+            System.out.println("Enter an item name or type -1 to finish");
+            String userInput = scan.nextLine();
+
+            if (userInput.equals("-1")){
+                endCode = true;
+            }else{
+                items += userInput + "\n"; 
+            }
+        }
+
+        result.append("Items ordered:\n");
+        result.append(items);
         return result.toString();
     }
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
-        int people = 12; 
-        int percent = 15;
-        double cost = 566.97;              
+        int people = 6; 
+        int percent = 25;
+        double cost = 52.27;              
         System.out.println(calculateTip(people, percent, cost));
     }
 }
-        
